4b2e93eeb710db73162bde0ce722ff7392749a45
Daniel Berg <danielbergmorais@gmail.com>
2025-01-03 16:12:52 -0300

    Adicionado função login basica no frontend

--- frontend/src/Components/LoginForm/LoginForm.jsx
+++ frontend/src/Components/LoginForm/LoginForm.jsx
@@ -1,26 +1,121 @@
-import React from 'react';
-import './LoginForm.css';
+import React, { useState, useEffect }  from 'react';
 import { FaUser, FaLock } from 'react-icons/fa';
+import axios from "axios";
+import './LoginForm.css';
+
 
 const LoginForm = () => {
+    const initialValues = {
+        email: "",
+        password: ""
+    };
+
+    const [formValues, setFormValues] = useState(initialValues);
+    const [isSubmit, setIsSubmit] = useState(false);
+    const [formErrors, setFormErrors] = useState({});
+    const [formSuccess, setFormSuccess] = useState('');
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormValues({...formValues, [name]: value});
+    };
+
+    const handeSubmit = async (e) => {
+        e.preventDefault();
+        setFormErrors(validate(formValues));
+        setIsSubmit(true);
+        if(Object.keys(formErrors).length === 0) {
+            try {
+
+                const jsonData = JSON.stringify(formValues);
+                const customConfig = {
+                    headers: {
+                    'Content-Type': 'application/json'
+                    }
+                };
+                const response = await axios.post('http://localhost:4000/auth/signin', jsonData, customConfig);
+
+                if(response.data && response.data.success) {
+                    setFormSuccess(response.data.message);
+                }
+
+            } catch (err) {
+                console.log(err.response.data)
+                if( err.response.data && !err.response.success){
+                    setFormErrors({login: err.response.data.message});
+                }
+            }
+        }
+    };
+
+    const validate = (values) => {
+        
+        const errors = {};
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+
+        if (!values.email) {
+            errors.email = "Email é obrigatorio!";
+        } else if (!regex.test(values.email)) {
+            errors.email = "Este formato é inválido para email!";
+        }
+        if (!values.password) {
+            errors.password = "Senha é obrigatória!";
+        } else if (values.password.length < 4) {
+            errors.password = "Senha deve conter mais de 4 caracteres.";
+        }
+        return errors;
+    };
+
+    useEffect(() => {
+        console.log(formErrors);
+        if (Object.keys(formErrors).length === 0 && isSubmit) {
+            console.log(formValues);
+        }
+    }, [formErrors, formValues, isSubmit]);
+
     return (
         <div className='wrapper'>
-            <form action="" method='post'>
+            <form action="" method='post' onSubmit={handeSubmit}>
                 <h1>Login</h1>
                 <div className="input-box">
-                    <input type="email" name="login" id="login" placeholder='E-mail' required />
+                    <input 
+                        type="email" 
+                        name="email" 
+                        id="email" 
+                        placeholder='E-mail' 
+                        required 
+                        value={formValues.email}
+                        onChange={handleChange}
+                        />
                     <FaUser className='icon' />
                 </div>
+                <p className='error-login' >
+                        {formErrors.email}
+                    </p>
                 <div className="input-box">
-                    <input type="password" name="password" id="password" placeholder='Senha' required />
+                    <input 
+                        type="password" 
+                        name="password" 
+                        id="password" 
+                        placeholder='Senha' 
+                        required 
+                        value={formValues.password}
+                        onChange={handleChange}
+                        />
                     <FaLock className='icon'/>
+                 
                 </div>
-
-                <div className="remeber-forgot">
-                    <label> <input type="checkbox" name="remeber" id="remember" /> Lembrar senha</label>
+                <p className="login-success">
+                    {formSuccess}
+                </p>
+                <p className='error-login' >
+                    {formErrors.password} {formErrors.login}
+                </p>
+                <div className="remember-forgot">
+                    <label> <input type="checkbox" name="remember" id="remember" /> Lembrar senha</label>
                     <a href='#'>Esqueci minha senha</a>
                 </div>
-                <p className='error-login' style={{display: 'none'}}>Email ou Senha incorretos!</p>
+              
                 <button type="submit">
                     Entrar
                 </button>
